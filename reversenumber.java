f0328d09642f6077d515972b352e92029cf7ac56
ANKUSH <ankushchoudhary2019@gmail.com>
2026-02-20 07:59:19 -0800

    Add reverse number program

---
+++ reversenumber.java
@@ -0,0 +1,13 @@
+public class reversenumber{
+    public static void main(String[] args) {
+        int n = 635482;
+        int rev = 0;
+
+        while (n != 0 ) {
+           int digit= n%10;
+           rev= rev*10 + digit;
+           n=n/10; 
+        }
+        System.out.println(rev);
+    }
+}
